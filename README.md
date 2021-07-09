2c7271f8ac9a3aac00b142fc0dc133a21fa71a05
Cyclic3 <cyclic3.git@gmail.com>
2021-07-09 18:51:17 +0100

    Added standard templates

---
+++ README.md
@@ -0,0 +1,18 @@
+# csmeme
+This abuses the already broken playerradio command to do many fun things.
+
+## Common usage
+Run `python3 main.py` with the following arguments to get the message put in your clipboard!
+
+| Arguments | Effects |
+| `T_HAX Cyclic3` | Creates a blatently fake VAC message for Cyclic3, complete with haha funny noise A|
+| `T_VAC Cyclic3` | Creates a more believeable VAC message for Cyclic3 |
+| `T_CASE Cyclic3` | Gives Cyclic3 a Dragon Lore from a case! |
+| `T_STAT Cyclic3` | Gives Cyclic3 a StatTrak Dragon Lore from a case (which defo exist) |
+| `T_FTS Cyclic3` | Abandons Cyclic3 |
+| `T_SUDO Cyclic3 "I hate you all"` | Makes Cyclic3 say "I hate you all" |
+| `T_SUDOT Cyclic3 "I hate you all"` | Same as `T_SUDO` but for T team chat |
+| `T_SUDOCT Cyclic3 "I hate you all"` | Same as `T_SUDO` but for CT team chat |
+
+## Advanced usage
+I also have a bunch of building blocks that can be used to create your own message, but the sytax varies frequently
--- main.py
+++ main.py
@@ -18,7 +18,10 @@ def process_one(args: list) -> Optional[str]:
     if len(args) == 0:
         return None
     keyword = args.pop(0).upper()
-    return handlers.get(keyword)(args)
+    fun = handlers.get(keyword)
+    if fun is None:
+        raise Exception("Invalid command: " + keyword)
+    return fun(args)
 
 
 def process_all(args: list) -> str:
@@ -31,23 +34,32 @@ def process_all(args: list) -> str:
     return text
 
 handlers = {
-    "SILENT": lambda x: f"""playerradio Radio.WePlanted "{x.pop(0)}\u2028""",
-    "CRINGE": lambda x: f"""playerradio DeathCry "{x.pop(0)}\u2028""",
-    "HAX": lambda _: """playerradio DeathCry "CHUNGUS.EXITFRAG.RU\u2028""",
+    "T_HAX": lambda x: process_all(["CRINGE", "TEXT", "CHUNGUSAIM.RU", "NEWLINE", "RED", "TEXT", f"{x.pop(0)} has been permanently banned from official CS:GO servers."]),
+    "T_VAC": lambda x: process_all(["SILENT", "NEWLINE", "RED", "TEXT", f"{x.pop(0)} has been permanently banned from official CS:GO servers."]),
+    "T_CASE": lambda x: process_all(["SILENT", "NEWLINE", "CASE", x.pop(0), "RED", "AWP", "Dragon Lore"]),
+    "T_STAT": lambda x: process_all(["SILENT", "NEWLINE", "STAT", x.pop(0), "RED", "AWP", "Dragon Lore"]),
+    "T_FTS": lambda x: process_all(["SILENT", "NEWLINE", "ABANDON", x.pop(0), "7 day"]),
+    "T_SUDO": lambda x: process_all(["SILENT", "NEWLINE", "DEFAULT", "TEXT", f'{x.pop(0)} : ', "WHITE", "TEXT", x.pop(0) ]),
+    "T_SUDOCT": lambda x: process_all(["SILENT", "NEWLINE", "DEFAULT", "TEXT", f'(Counter-Terrorist) {x.pop(0)} : ', "WHITE", "TEXT", x.pop(0)]),
+    "T_SUDOT": lambda x: process_all(["SILENT", "NEWLINE", "DEFAULT", "TEXT", f'(Terrorist) {x.pop(0)} : ', "WHITE", "TEXT", x.pop(0)]),
+
+    "SILENT": lambda x: f'playerradio Radio.WePlanted "',
+    "CRINGE": lambda x: f'playerradio DeathCry "',
+
     "TEXT": lambda x: x.pop(0),
-    "TRADE": lambda x: process_all(["ICON", "TEXT", x.pop(0), "WHITE", "TEXT", " has received in trade: ", x.pop(0), "TEXT", x.pop(0) + " | " + x.pop(0)]),
-    "CASE": lambda x: process_all(["ICON", "TEXT", x.pop(0), "WHITE", "TEXT", " has opened a container and found: ", x.pop(0), "TEXT", x.pop(0) + " | " + x.pop(0)]),
-    "STAT": lambda x: process_all(["ICON", "TEXT", x.pop(0), "WHITE", "TEXT", " has opened a container and found: ", x.pop(0), "TEXT", "StatTrak™ " + x.pop(0) + " | " + x.pop(0)]),
-    "STAR": lambda x: process_all(["ICON", "TEXT", x.pop(0), "WHITE", "TEXT", " has opened a container and found: ", x.pop(0), "TEXT", "★ " + x.pop(0) + " | " + x.pop(0)]),
-    "VAC": lambda x: process_all(["RED", "TEXT", f"{x.pop(0)} has been permanently banned from official CS:GO servers."]),
+    "TRADE": lambda x: process_all(["TEXT", x.pop(0), "WHITE", "TEXT", " has received in trade: ", x.pop(0), "TEXT", x.pop(0) + " | " + x.pop(0)]),
+    "CASE": lambda x: process_all(["TEXT", x.pop(0), "WHITE", "TEXT", " has opened a container and found: ", x.pop(0), "TEXT", x.pop(0) + " | " + x.pop(0)]),
+    "STAT": lambda x: process_all(["TEXT", x.pop(0), "WHITE", "TEXT", " has opened a container and found: ", x.pop(0), "TEXT", "StatTrak™ " + x.pop(0) + " | " + x.pop(0)]),
+    "STAR": lambda x: process_all(["TEXT", x.pop(0), "WHITE", "TEXT", " has opened a container and found: ", x.pop(0), "TEXT", "★ " + x.pop(0) + " | " + x.pop(0)]),
+    "VAC": lambda x: process_all([]),
     "ABANDON": lambda x: process_all(["RED", "TEXT", f"{x.pop(0)} abandoned the match and received a {x.pop(0)} competitive matchmaking cooldown."]),
     "NEWLINE": lambda _: '\u2028',
+    "COL": lambda x: chr(x.pop()),
+    "DEFAULT": lambda _: '\x03',
+    "RED": lambda _: '\x07',
     "WHITE": lambda _: '\x01',
-    "ICON": lambda _: '\x03',
-    "RED": lambda _: '\x0F',
     # "KNIFE": lambda _: proce
 }
-
 
 if __name__ == "__main__":
     args = list(sys.argv[1:])
